eae5441e7bc4c45473e12eef42029e1b147bc809
Jeancy Tshibemba Mukendi <jeancyts@gmail.com>
2021-04-25 10:40:49 +0200

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -4,24 +4,30 @@ public class Palindrome {
   
     // Function that returns true if 
     // str is a palindrome 
+  
+    // A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward, such as madam or racecar.
     static boolean isPalindrome(String str) 
     { 
   
         // Pointers pointing to the beginning 
         // and the end of the string 
-        int i = 0, j = str.length() - 1; 
+        int i = 0;
+        int j = str.length() - 1; 
   
         // While there are characters to compare 
         while (i < j) { 
   
             // If there is a mismatch 
-            if (str.charAt(i) != str.charAt(j)) 
+            if (str.charAt(i) != str.charAt(j)) {
                 return false; 
-  
-            // Increment first pointer and 
-            // decrement the other 
-            i++; 
-            j--; 
+            }
+            // By the time i becomes greater than j it means we have managed to compare all element to each other respectively and beeing this far means the string is a palindrome
+            if(i>j){
+               return true;
+            }
+             
+            i++; // Increment first pointer and
+            j--; // decrement the other 
         } 
   
         // Given string is a palindrome 
